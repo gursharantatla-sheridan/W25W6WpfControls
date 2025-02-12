0ce4a6ebe81eca8938b170964060dab5fbbe7ec4
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-02-12 15:17:35 -0500

    Add project files.

---
+++ W25W6WpfControls/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace W25W6WpfControls;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
